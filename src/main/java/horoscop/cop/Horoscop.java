330e331518c9ff09c4c96aa0e6e7c8f1904e1dc7
Mateoacevedo15 <mateo.acevedom.col@upb.edu.co>
2022-05-09 22:26:28 -0500

    version1

---
+++ src/main/java/horoscop/cop/Horoscop.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package horoscop.cop;
+
+/**
+ *
+ * @author Mateo Acevedo
+ */
+public class Horoscop {
+
+    public static void main(String[] args) {
+        
+    }
+}
